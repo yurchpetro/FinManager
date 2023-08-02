07353a7385dce665c0e8b7b3b4401acca0b9726d
user <petro.yurchyshyn.wk@gmail.com>
2023-08-02 10:26:34 +0300

    use transaction item

--- src/pages/dashboard/dashboard.module.ts
+++ src/pages/dashboard/dashboard.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { CommonModule } from '@angular/common';
+import { DashboardUiModule } from '@libs/dashboard/ui/dashboard-ui.module';
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, DashboardRoutingModule],
+    imports: [CommonModule, DashboardRoutingModule, DashboardUiModule],
     providers: [],
 })
 export class DashboardModule {}
