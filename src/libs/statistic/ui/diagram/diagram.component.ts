98972012ef673d1a962677d9e5f0006b18a08b99
user <petro.yurchyshyn.wk@gmail.com>
2023-08-03 13:46:38 +0300

    use chart js

--- src/libs/statistic/ui/diagram/diagram.component.ts
+++ src/libs/statistic/ui/diagram/diagram.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Chart, ChartItem, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
     selector: 'app-diagram',
@@ -6,4 +8,31 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     styleUrls: ['./diagram.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DiagramComponent {}
+export class DiagramComponent implements OnInit {
+    private chart: any;
+
+    public ngOnInit(): void {
+        this.createChart();
+    }
+
+    createChart(): void {
+        const ctx: ChartItem = document.getElementById('myChart') as HTMLCanvasElement;
+        this.chart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Label 1', 'Label 2', 'Label 3'],
+                datasets: [
+                    {
+                        label: 'My First Dataset',
+                        data: [300, 50, 100],
+                        backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)'],
+                        hoverOffset: 4,
+                    },
+                ],
+            },
+            options: {
+                // Chart options and configurations go here
+            },
+        });
+    }
+}
